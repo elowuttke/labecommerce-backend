cac9da469b9fdff9a50ea966332b205c1fff452a
elowuttke <elowuttke@gmail.com>
2023-03-28 14:56:07 -0300

    exercícios do typescript I DONE

---
+++ build/database.js
@@ -0,0 +1,83 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.purchases = exports.products = exports.users = void 0;
+exports.users = [
+    {
+        id: "1001",
+        email: "felipe@labenu.com",
+        password: "123456",
+    },
+    {
+        id: "1002",
+        email: "fernanda@labenu.com",
+        password: "234567",
+    },
+    {
+        id: "1003",
+        email: "fabio@labenu.com",
+        password: "345678",
+    },
+    {
+        id: "1004",
+        email: "felicia@labenu.com",
+        password: "456789",
+    },
+    {
+        id: "1005",
+        email: "fabiana@labenu.com",
+        password: "567890",
+    }
+];
+exports.products = [
+    {
+        id: "2001",
+        name: "Feijão",
+        price: 10,
+        category: "alimento"
+    },
+    {
+        id: "2002",
+        name: "Farofa",
+        price: 7,
+        category: "alimento"
+    },
+    {
+        id: "2003",
+        name: "Figo",
+        price: 20,
+        category: "alimento"
+    },
+    {
+        id: "2004",
+        name: "Faca",
+        price: 3,
+        category: "bazar"
+    },
+    {
+        id: "2005",
+        name: "Festão",
+        price: 40,
+        category: "bazar"
+    },
+];
+exports.purchases = [
+    {
+        userId: "1001",
+        productId: "2005",
+        quantity: 3,
+        totalPrice: 120
+    },
+    {
+        userId: "1002",
+        productId: "2002",
+        quantity: 2,
+        totalPrice: 14
+    },
+    {
+        userId: "1004",
+        productId: "2001",
+        quantity: 5,
+        totalPrice: 50
+    },
+];
+//# sourceMappingURL=database.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { users, products, purchases } from "./database"
+
+console.log("rodou o arquivo index.ts")
+console.log(users)
+console.log(products)
+console.log(purchases)
