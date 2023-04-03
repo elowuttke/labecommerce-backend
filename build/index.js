ae1266a2f0bdfb383da0a4a35e66a3ae749eb3ff
elowuttke <elowuttke@gmail.com>
2023-04-03 16:12:27 -0300

    get ping-users-products-productByName post user-product-purchase

--- build/index.js
+++ build/index.js
@@ -1,7 +1,24 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const database_1 = require("./database");
 const types_1 = require("./types");
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
+app.listen(3003, () => {
+    console.log("Servidor rodando na porta 3003");
+});
+app.get('/ping', (req, res) => {
+    res.send('Pong');
+});
+app.get('/users', (req, res) => {
+    res.status(200).send(database_1.users);
+});
 (0, database_1.createUser)("1006", "felix@labenu.com", "678901");
 (0, database_1.createProduct)("2006", "Fita k7", 15, types_1.CATEGORY.ELETRONICS);
 (0, database_1.getProductById)("2003");
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,21 @@
-// import express, { Request, Response } from 'express'
-// import cors from 'cors'
+import express, { Request, Response } from 'express'
+import cors from 'cors'
 import { users, products, purchases, createUser, createProduct, getProductById } from "./database"
-import { CATEGORY } from "./types"
+import { CATEGORY, TProduct, TPurchase, TUser } from "./types"
 
-// const app = express()
+const app = express();
 
-// app.use(express.json())
-// app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-// app.listen(3003, () => {
-//     console.log("Servidor rodando na porta 3003")
-// })
+app.listen(3003, () => {
+    console.log("Servidor rodando na porta 3003")
+});
 
-// app.get("/ping", (req: Request, res: Response) => {
-//     res.send("Pong!")
-// })
+// PING
+app.get('/ping', (req: Request, res: Response) => {
+    res.send('Pong!')
+})
 
 createUser("1006", "felix@labenu.com", "678901")
 
@@ -22,7 +23,62 @@ createProduct("2006", "Fita k7", 15, CATEGORY.ELETRONICS)
 
 getProductById("2003")
 
-//console.log("rodou o arquivo index.ts")
-//console.log(users)
-//console.log(products)
-//console.log(purchases)
+// GET ALL USERS
+app.get('/users', (req: Request, res: Response) => {
+    res.status(200).send(users)
+})
+
+// GET ALL PRODUCTS
+app.get('/products', (req: Request, res: Response) => {
+    res.status(200).send(products)
+})
+
+// GET PRODUCT BY NAME
+app.get('/product/search', (req: Request, res: Response) => {
+    const q = req.query.q as string
+    const result = products.filter((product) => {
+        return product.name.toLowerCase().includes(q.toLowerCase())
+    })
+    res.status(200).send(result)
+})
+
+// POST NEW USER
+app.post('/users', (req: Request, res: Response) => {
+    const id = req.body.id
+    const email = req.body.email
+    const password = req.body.password
+
+    const newUser: TUser = { id, email, password }
+    users.push(newUser)
+    //console.log(users)
+
+    res.status(201).send('Cadastro realizado com sucesso')
+})
+
+// POST NEW PRODUCT
+app.post('/products', (req: Request, res: Response) => {
+    const id = req.body.id
+    const name = req.body.name
+    const price = req.body.price
+    const category = req.body.category
+
+    const newProduct: TProduct = { id, name, price, category }
+    products.push(newProduct)
+    //console.log(products)
+
+    res.status(201).send('Produto cadastrado com sucesso')
+})
+
+// POST NEW PURCHASE
+app.post('/purchases', (req: Request, res: Response) => {
+    const userId = req.body.userId
+    const productId = req.body.productId
+    const quantity = req.body.quantity
+    const totalPrice = req.body.totalPrice
+
+    const newPurchase: TPurchase = { userId, productId, quantity, totalPrice }
+    purchases.push(newPurchase)
+    console.log(purchases)
+
+    res.status(201).send('Compra realizada com sucesso')
+})
